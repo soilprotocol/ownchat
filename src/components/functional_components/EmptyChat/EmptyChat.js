89014377199578487ee446b38c04f25a391196be
Bejowe <fast.pfeil@googlemail.com>
2019-05-04 16:55:12 +0200

    Refactor friendslot

--- src/components/functional_components/EmptyChat/EmptyChat.js
+++ src/components/functional_components/EmptyChat/EmptyChat.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import styles from './EmptyChat.module.css';
 
 const EmptyChat = () => {
   return (
     <Row>
-      <Col lg />
-      <Col lg="6">
-        <h1 style={{ margin: "10%" }}>
+      <Col sm lg />
+      <Col sm="6" md="6" lg="6">
+        <h1 className={styles.text}>
           Select a friend to start chatting
         </h1>
       </Col>
-      <Col lg />
+      <Col sm lg />
     </Row>
   );
 };
--- src/components/functional_components/FriendSlot/FriendSlot.js
+++ src/components/functional_components/FriendSlot/FriendSlot.js
@@ -1,56 +1,27 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
+import styles from './FriendSlot.module.css';
 
 const FriendSlot = props => {
   const friend = props.friend;
   return (
-    <Row
-      style={{
-        width: "100%",
-        paddingBottom: "20px"
-      }}
+    <div
+      className={styles.container}
       index={props.index}
       webid={friend.webId}
     >
-      <Col lg="3" style={{ width: "20%" }}>
-        <div
-          style={{
-            overflow: "hidden",
-            borderRadius: "50%",
-            width: "100px",
-            height: "100px"
-          }}
-        >
-          <Image
-            style={{
-              maxWidth: "150px",
-              maxHeight: "150px",
-              display: "block",
-              marginLeft: "auto",
-              marginRight: "auto"
-            }}
-            src={friend.picture}
-            alt={"Profile picture of " + friend.name}
-            webid={friend.webId}
-          />
-        </div>
-      </Col>
-      <Col
-        lg="8"
-        style={{
-          marginTop: "auto",
-          marginBottom: "20%",
-          display: "block",
-          width: "80%",
-          paddingLeft: "20%"
-        }}
+      <div 
+        className={styles.image}
+        style={{backgroundImage:`url(${friend.picture})`}}
+      />
+      <strong 
+        className={styles.nameText}
+        index={props.index}
+        webid={friend.webId}
       >
-        <strong index={props.index} webid={friend.webId}>{friend.name}</strong>
-      </Col>
-      <Col lg />
-    </Row>
+      {friend.name}
+      </strong>
+    </div>
   );
 };
 
